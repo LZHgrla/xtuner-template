33a4002da110ba4f58167346d2db9e9a24f4b43f
LZHgrla <linzhihao@pjlab.org.cn>
2024-01-26 16:37:32 +0800

    update

--- configs/demo.py
+++ configs/demo.py
@@ -9,7 +9,6 @@ from peft import LoraConfig
 from torch.optim import AdamW
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
                           BitsAndBytesConfig)
-
 from xtuner.dataset import process_hf_dataset
 from xtuner.dataset.collate_fns import default_collate_fn
 from xtuner.dataset.map_fns import alpaca_map_fn, template_map_fn_factory
@@ -56,88 +55,82 @@ evaluation_inputs = [
 #######################################################################
 #                      PART 2  Model & Tokenizer                      #
 #######################################################################
-tokenizer = dict(
-    type=AutoTokenizer.from_pretrained,
-    pretrained_model_name_or_path=pretrained_model_name_or_path,
-    trust_remote_code=True,
-    padding_side='right')
+tokenizer = dict(type=AutoTokenizer.from_pretrained,
+                 pretrained_model_name_or_path=pretrained_model_name_or_path,
+                 trust_remote_code=True,
+                 padding_side='right')
 
-model = dict(
-    type=SupervisedFinetune,
-    llm=dict(
-        type=AutoModelForCausalLM.from_pretrained,
-        pretrained_model_name_or_path=pretrained_model_name_or_path,
-        trust_remote_code=True,
-        torch_dtype=torch.float16,
-        quantization_config=dict(
-            type=BitsAndBytesConfig,
-            load_in_4bit=True,
-            load_in_8bit=False,
-            llm_int8_threshold=6.0,
-            llm_int8_has_fp16_weight=False,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type='nf4')),
-    lora=dict(
-        type=LoraConfig,
-        r=64,
-        lora_alpha=16,
-        lora_dropout=0.1,
-        bias='none',
-        task_type='CAUSAL_LM'))
+model = dict(type=SupervisedFinetune,
+             llm=dict(
+                 type=AutoModelForCausalLM.from_pretrained,
+                 pretrained_model_name_or_path=pretrained_model_name_or_path,
+                 trust_remote_code=True,
+                 torch_dtype=torch.float16,
+                 quantization_config=dict(type=BitsAndBytesConfig,
+                                          load_in_4bit=True,
+                                          load_in_8bit=False,
+                                          llm_int8_threshold=6.0,
+                                          llm_int8_has_fp16_weight=False,
+                                          bnb_4bit_compute_dtype=torch.float16,
+                                          bnb_4bit_use_double_quant=True,
+                                          bnb_4bit_quant_type='nf4')),
+             lora=dict(type=LoraConfig,
+                       r=64,
+                       lora_alpha=16,
+                       lora_dropout=0.1,
+                       bias='none',
+                       task_type='CAUSAL_LM'))
 
 #######################################################################
 #                      PART 3  Dataset & Dataloader                   #
 #######################################################################
-alpaca_en = dict(
-    type=process_hf_dataset,
-    dataset=dict(type=load_dataset, path=alpaca_en_path),
-    tokenizer=tokenizer,
-    max_length=max_length,
-    dataset_map_fn=alpaca_map_fn,
-    template_map_fn=dict(
-        type=template_map_fn_factory, template=prompt_template),
-    remove_unused_columns=True,
-    shuffle_before_pack=True,
-    pack_to_max_length=pack_to_max_length)
+alpaca_en = dict(type=process_hf_dataset,
+                 dataset=dict(type=load_dataset, path=alpaca_en_path),
+                 tokenizer=tokenizer,
+                 max_length=max_length,
+                 dataset_map_fn=alpaca_map_fn,
+                 template_map_fn=dict(type=template_map_fn_factory,
+                                      template=prompt_template),
+                 remove_unused_columns=True,
+                 shuffle_before_pack=True,
+                 pack_to_max_length=pack_to_max_length)
 
-train_dataloader = dict(
-    batch_size=batch_size,
-    num_workers=dataloader_num_workers,
-    dataset=alpaca_en,
-    sampler=dict(type=DefaultSampler, shuffle=True),
-    collate_fn=dict(type=default_collate_fn))
+train_dataloader = dict(batch_size=batch_size,
+                        num_workers=dataloader_num_workers,
+                        dataset=alpaca_en,
+                        sampler=dict(type=DefaultSampler, shuffle=True),
+                        collate_fn=dict(type=default_collate_fn))
 
 #######################################################################
 #                    PART 4  Scheduler & Optimizer                    #
 #######################################################################
 # optimizer
-optim_wrapper = dict(
-    type=AmpOptimWrapper,
-    optimizer=dict(
-        type=optim_type, lr=lr, betas=betas, weight_decay=weight_decay),
-    clip_grad=dict(max_norm=max_norm, error_if_nonfinite=False),
-    accumulative_counts=accumulative_counts,
-    loss_scale='dynamic',
-    dtype='float16')
+optim_wrapper = dict(type=AmpOptimWrapper,
+                     optimizer=dict(type=optim_type,
+                                    lr=lr,
+                                    betas=betas,
+                                    weight_decay=weight_decay),
+                     clip_grad=dict(max_norm=max_norm,
+                                    error_if_nonfinite=False),
+                     accumulative_counts=accumulative_counts,
+                     loss_scale='dynamic',
+                     dtype='float16')
 
 # learning policy
 # More information: https://github.com/open-mmlab/mmengine/blob/main/docs/en/tutorials/param_scheduler.md  # noqa: E501
 param_scheduler = [
-    dict(
-        type=LinearLR,
-        start_factor=1e-5,
-        by_epoch=True,
-        begin=0,
-        end=warmup_ratio * max_epochs,
-        convert_to_iter_based=True),
-    dict(
-        type=CosineAnnealingLR,
-        eta_min=0.0,
-        by_epoch=True,
-        begin=warmup_ratio * max_epochs,
-        T_max=max_epochs,
-        convert_to_iter_based=True)
+    dict(type=LinearLR,
+         start_factor=1e-5,
+         by_epoch=True,
+         begin=0,
+         end=warmup_ratio * max_epochs,
+         convert_to_iter_based=True),
+    dict(type=CosineAnnealingLR,
+         eta_min=0.0,
+         by_epoch=True,
+         begin=warmup_ratio * max_epochs,
+         T_max=max_epochs,
+         convert_to_iter_based=True)
 ]
 
 # train, val, test setting
@@ -149,13 +142,12 @@ train_cfg = dict(type=TrainLoop, max_epochs=max_epochs)
 # Log the dialogue periodically during the training process, optional
 custom_hooks = [
     dict(type=DatasetInfoHook, tokenizer=tokenizer),
-    dict(
-        type=EvaluateChatHook,
-        tokenizer=tokenizer,
-        every_n_iters=evaluation_freq,
-        evaluation_inputs=evaluation_inputs,
-        system=SYSTEM,
-        prompt_template=prompt_template)
+    dict(type=EvaluateChatHook,
+         tokenizer=tokenizer,
+         every_n_iters=evaluation_freq,
+         evaluation_inputs=evaluation_inputs,
+         system=SYSTEM,
+         prompt_template=prompt_template)
 ]
 
 # configure default hooks
@@ -167,11 +159,10 @@ default_hooks = dict(
     # enable the parameter scheduler.
     param_scheduler=dict(type=ParamSchedulerHook),
     # save checkpoint per `save_steps`.
-    checkpoint=dict(
-        type=CheckpointHook,
-        by_epoch=False,
-        interval=save_steps,
-        max_keep_ckpts=save_total_limit),
+    checkpoint=dict(type=CheckpointHook,
+                    by_epoch=False,
+                    interval=save_steps,
+                    max_keep_ckpts=save_total_limit),
     # set sampler seed in distributed evrionment.
     sampler_seed=dict(type=DistSamplerSeedHook),
 )
@@ -203,4 +194,3 @@ randomness = dict(seed=None, deterministic=False)
 
 # set log processor
 log_processor = dict(by_epoch=False)
-
